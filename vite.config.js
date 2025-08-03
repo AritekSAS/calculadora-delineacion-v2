fce5fa28df380b107c6e864e75c10187e513f19b
Arq_Maco <ariteksas@gmail.com>
2025-08-03 17:07:51 -0500

    ajustes para visualizacion

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 export default defineConfig({
+  root: resolve(__dirname, 'templates'),
   plugins: [react()],
   build: {
-    outDir: 'dist',
+    outDir: '../dist',
     assetsDir: '.',
     rollupOptions: {
-      input: 'src/main.jsx',
+      input: resolve(__dirname, 'templates/src/main.jsx'),
       output: {
         entryFileNames: 'bundle.js'
       }
